5caacba99c93b16c5865664b575e227ffe5c26a1
Emmanuel Nkaja <96911514+codenameMANUEL@users.noreply.github.com>
2023-06-13 17:49:45 +0100

    unique

--- resources/views/unique-process.blade.php
+++ resources/views/unique-process.blade.php
@@ -1,6 +1,6 @@
 @extends('templates.public')
 
- 
+
 @section('content')
 
 
@@ -8,88 +8,286 @@
     <h1 class="about_header">Blackstone Growth</h1>
     <p class="container about_paragraph">Growth Equity Reimagined®: Through revenue acceleration opportunities, operational support and access to Blackstone’s unparalleled resources, we’re redefining growth equity.</p>
     <div class="vertical_line">
-    <span class="vl"></span>
+        <span class="vl"></span>
     </div>
     <div class="learn_more_about">
-    <a href="#" class="">LEARN MORE</a>
+        <a href="#" class="">LEARN MORE</a>
     </div>
-    
+
 </div>
 
 
 <div class="container section_two_about">
-        <p class="section_two_paragraph">WHAT WE BUILD</p>
-        <h2 class="section_two_head">We invest in the companies shaping the future, helping emerging leaders become global champions through the power of the Blackstone platform.</h2>
+    <p class="section_two_paragraph">WHAT WE BUILD</p>
+    <h2 class="section_two_head">We invest in the companies shaping the future, helping emerging leaders become global champions through the power of the Blackstone platform.</h2>
 
-        <div class="container section_two_about_divs">
-            <div class="section_two_about_text_section">
-                <div>
+    <div class="container section_two_about_divs">
+        <div class="section_two_about_text_section">
+            <div>
                 <h2 class="section_two_txt">Leveraging our world-class platform…</h2>
                 <p>We are seasoned growth equity investors backed by the resources of the world’s largest alternative asset manager. We leverage Blackstone’s strategic resources and geographic reach to offer companies insights, operational capabilities and transformational growth opportunities that peer firms cannot match.</p>
-                <div class="hr_spacing"><hr></div>
+                <div class="hr_spacing">
+                    <hr>
                 </div>
-                <div>
+            </div>
+            <div>
                 <h2 class="section_two_txt txt_two">To help companies scale efficiently…</h2>
                 <p>We equip our companies with the tools and know-how to expand rapidly while minimizing operational strains. Blackstone’s 100+ senior operating professionals and advisors, including leading technology executives, help our companies develop pattern recognition, pursue new growth opportunities and expand their global footprints.</p>
-                <div class="hr_spacing"><hr></div>
+                <div class="hr_spacing">
+                    <hr>
                 </div>
-                <div>
+            </div>
+            <div>
                 <h2 class="section_two_txt txt_three">And become global champions.</h2>
                 <p>We have decades of experience building world-class companies and helping them reach the “end state” in terms of operational excellence. We can anticipate the unique operational pressures that growth-stage companies face and draw upon our global base of resources to address them.</p>
-                </div>
             </div>
-            <div class="img_section">
-                <img src="assets/img/unique-process/DSC_2383.webp" alt="" class="section_two_about_img">
-            </div>
+        </div>
+        <div class="img_section">
+            <img src="assets/img/unique-process/DSC_2383.webp" alt="" class="section_two_about_img">
+        </div>
+    </div>
+</div>
+
+<div class="bxg">
+    <div class="container">
+        <h2>Access to the Blackstone Ecosystem</h2>
+        <img src="assets/img/unique-process/bxg_img.jpg" alt="">
+        <p>All numbers are as of December 31, 2022 unless otherwise indicated. Operating professionals include Blackstone employees and senior advisors.</p>
+        <ul>
+            <li>*As of September 31, 2022.</li>
+            <li>**As of March 31, 2023.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container unique_section">
+    <h1>We don’t just identify market leaders – we help build them.</h1>
+    <h4>Leveraging experience drawn from decades of building businesses, we offer targeted support at the most critical moments in a company’s development. Our large team of operating partners and highly curated portfolio allow us to be a better partner to high-growth companies navigating execution risk.</h4>
+</div>
+
+<div id="full-slider-wrapper">
+    <div id="layerslider" style="width:100%;height:600px;">
+        <!-- first slide -->
+        <div class="ls-slide" data-ls="slidedelay: 5000; transition2d:85;">
+            <img src="assets/img/unique-process/slider_one.jpeg" class="ls-bg" alt="Slide background">
+            <h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
+            <p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
+                SARA BLAKELY <br>
+                FOUNDER AND EXECUTIVE CHAIRWOMAN, SPANX
+            </p>
+        </div>
+        <!-- second slide -->
+        <div class="ls-slide" data-ls="slidedelay:5000; transition2d:103;">
+            <img src="assets/img/unique-process/slider_two.jpeg" class="ls-bg" alt="Slide background">
+            <h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
+            <p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
+                WHITNEY WOLFE HERD <br>
+                FOUNDER AND CEO, BUMBLE
+            </p>
+        </div>
+        <!-- third slide -->
+        <div class="ls-slide" data-ls="slidedelay: 5000; transition2d:5;">
+            <img src="assets/img/unique-process/slider_three.jpeg" class="ls-bg" alt="Slide background">
+            <h3 class="ls-l slide_typo" style="top:45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
+            <p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
+                WHITNEY WOLFE HERD <br>
+                FOUNDER AND CEO, BUMBLE</p>
+        </div>
+    </div>
+</div>
+
+<div class="container unique_section">
+    <h1>Deep Sector Expertise</h1>
+    <h4>We focus on companies with proven business models across five sectors. We aim to partner with companies whose ambition and market opportunity have outgrown traditional growth equity firms but have not yet outgrown Blackstone.</h4>
+</div>
+<div class="container ">
+    <div class="row">
+        <div class="col-sm-4">
+            <a class="box_service" href="service-single.html">
+                <img src="assets/img/unique-process/cart.webp" alt="">
+                <h3>Consumer</h3>
+                <p>Partnering with the consumer “brands of tomorrow.”</p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a class="box_service" href="service-single.html">
+                <img src="assets/img/unique-process/cloud.webp" alt="">
+                <h3>Enterprise Software</h3>
+                <p>Supporting category-defining and durable franchise software and services.</p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a class="box_service" href="service-single.html">
+                <img src="assets/img/unique-process/chart.webp" alt="">
+                <h3>Financial Services</h3>
+                <p>Backing companies reshaping the financial system.</p>
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-5">
+            <a class="box_service" href="service-single.html">
+                <img src="assets/img/unique-process/healthcare.jpg" alt="">
+                <h3>Healthcare</h3>
+                <p>Investing in healthcare businesses that aim to reduce costs and improve outcomes.</p>
+            </a>
+        </div>
+        <div class="col-sm-2">
+        </div>
+        <div class="col-sm-5">
+            <a class="box_service" href="service-single.html">
+                <img src="assets/img/unique-process/mobilephone.webp" alt="">
+                <h3>Consumer Technology</h3>
+                <p>Building global leaders in consumer-facing technology.</p>
+            </a>
         </div>
     </div>
 
-        <div class="bxg">
-            <div class="container">
-            <h2>Access to the Blackstone Ecosystem</h2>
-            <img src="assets/img/unique-process/bxg_img.jpg" alt="">
-            <p>All numbers are as of December 31, 2022 unless otherwise indicated. Operating professionals include Blackstone employees and senior advisors.</p>
-            <ul>
-                <li>*As of September 31, 2022.</li>
-                <li>**As of March 31, 2023.</li>
-            </ul>
+    <div class="more_info">
+        <h1>Creating and Compounding Value</h1>
+        <p>Our long-term investment success comes from buying, building and holding great businesses for long periods of time.</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Focusing on Quality</h3>
+            <p>We acquire high-quality assets and businesses that can provide strong downside protection across market cycles</p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <h3>Being Patient and Ready</h3>
+            <p>We take a long-term view in deploying capital while acting decisively when the right opportunities emerge.</p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <h3>Taking a Contrarian View</h3>
+            <p>We see opportunities in assets, businesses, markets and sectors that are out of favor or in distress.</p>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+    <div class="container Sustainability_section">
+        <h1>Sustainability at Our Core</h1>
+        <p>We are more than simply an investor—we are active participants in industries and economies around the world. And we are committed to sustainability in everything we do.</p>
+    </div>
+
+    <div class="container">
+    <div class="bg_content Sustainability" id="skills">
+        <div class="container margin_120_95">
+            <div class="row">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6">
+                    <div class="content_center_Sustainability ">
+                        <h3>“Sustainability is in the fabric of our DNA. It is fundamental to our business and how we create value.”</h3>
+                        <p>BRUCE FLATT, CEO</p>
+                        <button class="btn_outline">Learn more about ESG at brookfield</button>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+    </div>
 
-        <div class="container unique_section">
-            <h1>We don’t just identify market leaders – we help build them.</h1>
-            <h4>Leveraging experience drawn from decades of building businesses, we offer targeted support at the most critical moments in a company’s development. Our large team of operating partners and highly curated portfolio allow us to be a better partner to high-growth companies navigating execution risk.</h4>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>Operational</h2>
+            <p>We understand a company’s potential and help enable it to sustain itself over the long term. We have successfully operated assets and businesses through multiple market cycles, building exceptional in-house knowledge, relationships and execution capabilities across sectors and regions.</p>
+            </a>
         </div>
-        
-        <div id="full-slider-wrapper">
-			<div id="layerslider" style="width:100%;height:600px;">
-				<!-- first slide -->
-				<div class="ls-slide" data-ls="slidedelay: 5000; transition2d:85;">
-					<img src="assets/img/unique-process/slider_one.jpeg" class="ls-bg" alt="Slide background">
-					<h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
-					<p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
-                    SARA BLAKELY <br>
-                    FOUNDER AND EXECUTIVE CHAIRWOMAN, SPANX
-					</p>
-				</div>
-				<!-- second slide -->
-				<div class="ls-slide" data-ls="slidedelay:5000; transition2d:103;">
-					<img src="assets/img/unique-process/slider_two.jpeg" class="ls-bg" alt="Slide background">
-					<h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
-					<p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
-                    WHITNEY WOLFE HERD <br>
-                    FOUNDER AND CEO, BUMBLE
-					</p>
-				</div>
-				<!-- third slide -->
-				<div class="ls-slide" data-ls="slidedelay: 5000; transition2d:5;">
-					<img src="assets/img/unique-process/slider_three.jpeg" class="ls-bg" alt="Slide background">
-					<h3 class="ls-l slide_typo" style="top:45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;">“Partnering with Blackstone is the best decision I’ve made in my career.”</h3>
-					<p class="ls-l slide_typo_2" style="top:55%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">
-                    WHITNEY WOLFE HERD <br>
-                    FOUNDER AND CEO, BUMBLE</p>
-				</div>
+        <div class="col-sm-4">
+            <h2>Financial</h2>
+            <p>We implement responsible capital and governance structures that support businesses for the long term. The concept starts with Brookfield and extends to our businesses and assets, and to our partnerships with investors. </p>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <h2>Environmental</h2>
+            <p>We are committed to managing our investments to be consistent with the transition to a net-zero economy by 2050. And throughout our operations, we strive to minimize our environmental impact and improve our efficient use of resources over time.</p>
+            </a>
+        </div>
+    </div>
+    </div>
+
+    <div class="container">
+    <div class=" margin_60_35">
+        <div class="owl-carousel owl-theme case_studies add_bottom_30">
+
+            <div class="renewable_carosel ">
+                <div class="box_service">
+                    <figure><img src="assets/img/unique-process/windwhril.webp" alt=""> </figure>
+                    <p><b class="text-primary">Renewable Power</b></p>
+                    <h1>An Onshore Wind Portfolio in One of Europe’s Strongest Wind Resource Locations</h1>
+                    <p>Ireland is a prime location for this wind portfolio, and a place where wind is a competitive source of electricity in a natural gas fired market.</p>
+                    <a href=""> Read More</a>
+                </div>
+            </div>
+
+            <div>
+            <div class="box_service">
+                    <figure><img src="assets/img/unique-process/solar.webp" alt=""> </figure>
+                    <p><b class="text-primary">Impact Investing</b></p>
+                    <h1>Powering the Transition to Net Zero</h1>
+                    <p>Net zero is not only a global imperative—it’s an attractive opportunity.</p>
+                    <a href=""> Read More</a>
+                </div>
+            </div>
+
+            <div>
+            <div class="box_service">
+                    <figure><img src="assets/img/unique-process/greenenegry.webp" alt=""> </figure>
+                    <p><b class="text-primary">Private Equity</b></p>
+                    <h1>Recycling Food Waste Sustainably</h1>
+                    <p>Greenergy has implemented an innovative sustainability project at its two biodiesel manufacturing facilities on the east coast of England.</p>
+                    <a href=""> Read More</a>
+                </div>
+            </div>
+
+            <div>
+            <div class="box_service">
+                    <figure><img src="assets/img/unique-process/greenenegry.webp" alt=""> </figure>
+                    <p><b class="text-primary">Infrastructure</b></p>
+                    <h1>Keeping Plastics out of Rivers and Seas</h1>
+                    <p>PD Ports is the first U.K. port to sign up to Operation Clean Sweep.</p>
+                    <a href=""> Read More</a>
+                </div>
+            </div>
+
+            <div>
+            <div class="box_service">
+                    <figure><img src="assets/img/unique-process/solar.webp" alt=""> </figure>
+                    <p><b class="text-primary">Real Estate</b></p>
+                    <h1>A First-of-its-kind Community Solar Initiative</h1>
+                    <p>Three rooftops have become the first downtown commercial locations to produce solar power for this purpose.</p>
+                    <a href=""> Read More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+
+    <div id="newsletter_container">
+		<div class="container margin_60">
+			<div class="row">
+                <div class="col-6">
+                    <img src="assets/img/unique-process/greenenegry.webp" alt="">
+                    <p>WHITE PAPER</p>
+                    <p>Q&A: Why Brookfield Private Equity</p>
+                </div>
+                <div class="col-3">
+                <img src="assets/img/unique-process/greenenegry.webp" alt="">
+                    <p>WHITE PAPER</p>
+                    <p>Q&A: Why Brookfield Private Equity</p>
+                </div>
+                <div class="col-3">
+                <img src="assets/img/unique-process/greenenegry.webp" alt="">
+                    <p>WHITE PAPER</p>
+                    <p>Q&A: Why Brookfield Private Equity</p>
+                </div>
 			</div>
 		</div>
+	</div>
 
-@endsection
+    @endsection
