72c19f4caf84b41339256053692ae289d18001fb
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-06-16 16:45:27 +0100

    responsible

--- resources/views/responsible-investing.blade.php
+++ resources/views/responsible-investing.blade.php
@@ -3,9 +3,26 @@
 @section('content') 
 
 
-<div class="container margin_60_35">
-    <h1>Responsible Investing PAGE</h1>
+<div class=" margin_60_35">
+    <div class="Responsible">
+        <div class="banner1">
+            <div class="text">
+                <h1>Small, Emerging, and Diverse <br> Manager Investing</h1>
+                <p>We serve as a catalyst for opportunity</p>
+            </div>
+            <div class="down">
+                <div class="rightwhite">
+                    <h2>$18.5 bn</h2>
+                    <p>small and emerging managers assets under management</p>
+                </div>
+                <div class="leftwhite">
+                    <h2>$14.5 bn</h2>
+                    <p>diverse managers assets under management</p>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 
  
 @endsection
