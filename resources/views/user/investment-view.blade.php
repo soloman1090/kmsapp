5e9516560c45a37a479675db7bfc6e3b5d211099
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-07-02 16:16:44 +0100

    investment

--- resources/views/user/investment-view.blade.php
+++ resources/views/user/investment-view.blade.php
@@ -12,8 +12,234 @@
         </nav>
         <h4 class="mg-b-0 tx-spacing--1">Coperate Investment Fund</h4>
     </div>
-     
-</div>
+</div>    
+<div class="Investment">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 ">
+                <div class="moon">
+                    <h1>Moonfare</h1>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="set1">
+                    <div class="card mb-3 p-3">
+                        <p>Portfolio Fund Trgets</p>
+                        <h1>~ 10</h1>
+                    </div>
+                    <div class="card mb-3 p-3">
+                        <p>Strategy Focus</p>
+                        <h1>Growth Equity</h1>
+                    </div>
+                    <div class="card mb-3 p-3">
+                        <p>Target Size</p>
+                        <h1>$95m</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="set2">
+            <span class="">Broad</span>
+            <div class="mb-4"></div>
+            <p>A growth portfolio providing access to selected top-tier growth equity and VC funds</p>
+        </div>
+        <div class="col-md-8">
+            <div class="card p-3">
+                <div class="set3">
+                    <ul>
+                        <li class="up">
+                            <p ><span class="greendot me-2"></span> IN FINAL CLOSE</p>
+                            <p> <span class="blue">INVESTMENT PROCESS</span> </p>
+                        </li>
+                        <li>
+                            <p class="grey">Strategy</p>
+                            <p> <span class="blue"> Growth Equity,</span> Portfolio</p>
+                        </li>
+                        <li>
+                            <p class="grey">Geography</p>
+                            <p>Global</p>
+                        </li>
+                        <li>
+                            <p class="grey">Fund Lifetime</p>
+                            <p> <span class="blue"> >12 years </span></p>
+                        </li>
+                        <li>
+                            <p class="grey">Investment Period</p>
+                            <p>Up to 2 years</p>
+                        </li>
+                        <li>
+                            <p class="grey"># of Investments</p>
+                            <p>~ 150</p>
+                        </li>
+                        <li>
+                            <p class="grey">Closing date</p>
+                            <p>Q4 2022</p>
+                        </li>
+                        <li>
+                            <p class="grey">Min. Investment</p>
+                            <p>$60,000</p>
+                        </li>
+                    </ul>
+                    <button class="nobackbtn mb-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-expand" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8ZM7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2ZM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10Z"/>
+                        </svg>Compare Fund
+                    </button> <br>
+                    <button class="withbackbtn mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                        </svg>
+                        Complete Profile
+                    </button>
+                    <center><p class="fsize10">Please complete your profile to unlock confidential fund information and request an allocation</p></center>
+                </div>
+            </div>
+        </div>
+        <div class="set4 margin50">
+            <h1>Investment Objectives</h1>
+            <div class="row margin40 ">
+                <div class="col-md-3">
+                    <h3>Intro to MCF</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Benefits to Investors</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Team</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+            </div>
+        </div>
+        <div class="set5 margin50">
+            <h1>Highlights</h1>
+            <div class=" margin40 ">
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+            </div>
+        </div>
+        <div class="set6 margin50">
+            <h1>Investment Objectives</h1>
+            <p class="margin30">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+            <div class="row margin40 ">
+                <div class="col-md-3">
+                    <h3>Seeking to outperform public markets</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Investing in top-performing buyout manager</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Providing diversified investment focus</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                </div>
+            </div>
+            <div class="row margin40 ">
+                <div class="col-md-3">
+                    <h3>Seeking to outperform public markets</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Investing in top-performing buyout manager</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="col-md-3">
+                    <h3>Providing diversified investment focus</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                </div>
+            </div>
+        </div>
+        <div class="set5 margin50">
+            <h1>Highlights</h1>
+            <h3 class=margin30>Investment Highlights</h3>
+            <div class=" margin40 ">
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+                <div class="opt1 mb-3">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                        </svg>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                </div>
+            </div>
+        </div>
+        <div class="set7 margin50">
+            <h1>Illustrative Portfolio Diversification</h1>
+            <div class="row margin40 ">
+                <div class="col-md-3">
+                    <p>Geography</p>
+                </div>
+                <div class="col-md-3">
+                    <p>Sector</p>
+                </div>
+                <div class="col-md-3">
+                    <p>Strategy</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> 
 
 
 
