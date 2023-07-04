c8eef9bc2ec7deacbfb151e1917a1eec44cb0cd6
Young Batimehin <135424342+soloman1090@users.noreply.github.com>
2023-07-04 18:45:52 +0100

    ds

--- resources/views/templates/main-user.blade.php
+++ resources/views/templates/main-user.blade.php
@@ -53,11 +53,11 @@
   <header class="navbar navbar-header navbar-header-fixed">
       <a href="" id="mainMenuOpen" class="burger-menu"><i data-feather="menu"></i></a>
       <div class="navbar-brand">
-        <a href="../../index.html" class="df-logo">dash<span>forge</span></a>
+        <a href="../../index.html" class="df-logo">Dell<span> Investment Group</span></a>
       </div><!-- navbar-brand -->
       <div id="navbarMenu" class="navbar-menu-wrapper">
         <div class="navbar-menu-header">
-          <a href="../../index.html" class="df-logo">dash<span>forge</span></a>
+          <a href="../../index.html" class="df-logo">Dell<span> Investment Group</span></a>
           <a id="mainMenuClose" href=""><i data-feather="x"></i></a>
         </div><!-- navbar-menu-header -->
         <ul class="nav navbar-menu">
