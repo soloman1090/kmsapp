b987a5cb2a48ea957dc2942616c924e64946fdd5
Young Batimehin <135424342+soloman1090@users.noreply.github.com>
2023-08-06 09:02:18 +0100

    up

--- resources/views/user/investment-view.blade.php
+++ resources/views/user/investment-view.blade.php
@@ -106,24 +106,15 @@
                                     <p>$60,000</p>
                                 </li>
                             </ul>
-                            {{-- <button class="nobackbtn mb-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-expand" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8ZM7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2ZM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10Z"/>
-                                </svg>Compare Fund
-                            </button> <br> --}}
+                            
                             <a href="/user/investment-payment">
                                 <button class="withbackbtn mb-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-                                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
-                                        </svg>
-                                        Complete Profile
+                                    <i class="bi bi-bag text-white"></i>
+                                    Purchase Investment
                                     </button>
                             </a>
-                             <button class="withbackbtn mb-3">
-                                <i class="bi bi-bag text-white"></i>
-                                Purchase Investment
-                            </button>
-                             <center><p class="fsize10">Please complete your profile to unlock confidential fund information and request an allocation</p></center>
-                        </div>
+                             
+                         </div>
                     </div>
                 </div>
             </div>
