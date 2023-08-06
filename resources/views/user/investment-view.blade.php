2d5b24baf3677055dda76e1758730a2275584876
Young Batimehin <135424342+soloman1090@users.noreply.github.com>
2023-08-06 09:29:45 +0100

    Update

--- resources/views/user/investment-view.blade.php
+++ resources/views/user/investment-view.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-30" id="upHeader">
-    <div class="" >
+    <div class="">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb breadcrumb-style1 mg-b-10">
                 <li class="breadcrumb-item"><a href="make-investment">Make Investment</a></li>
@@ -12,7 +12,7 @@
         </nav>
         <h4 class="mg-b-0 tx-spacing--1">Coperate Investment Fund</h4>
     </div>
-</div>    
+</div>
 <div class="Investment">
     <div class="container">
         <div class="row">
@@ -47,25 +47,157 @@
                         <p>A growth portfolio providing access to selected top-tier growth equity and VC funds</p>
                     </div>
                     <div class="set4 margin50">
-                        <h1> {!! $package->info_head_2 !!}</h1>
-                        {!! $package->info_detail_2 !!}
+                        <h1>Medium scale diversified investment portfolio</h1>
+                        <div class="margin40 row">
+                            <div class="col-md-4">
+                                <h3>Intro to MCF</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Benefits to Investors</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Team</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+                        </div>
+
                     </div>
                     <div class="set5 margin50">
-                        <h1> {!! $package->info_head_3 !!}</h1>
-                        {!! $package->info_detail_3 !!}
-                       
+                        <h1> Reimbursement Available</h1>
+                        <div class="margin40">
+                            <div class="mb-3 opt1">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="mb-3 opt1">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="mb-3 opt1">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="mb-3 opt1">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+                        </div>
+
+
                     </div>
                     <div class="set6 margin50">
-                        <h1> {!! $package->info_head_4 !!}</h1>
-                        {!! $package->info_detail_4 !!}
+                        <h1> Investment Objectives</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+
+                        <div class="margin40 row">
+                            <div class="col-md-4">
+                                <h3>Seeking to outperform public markets</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Investing in top-performing buyout manager</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Providing diversified investment focus</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                            </div>
+                        </div>
+
+                        <div class="margin40 row">
+                            <div class="col-md-4">
+                                <h3>Seeking to outperform public markets</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Investing in top-performing buyout manager</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Providing diversified investment focus</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                            </div>
+                        </div>
+
                     </div>
-                     
+
                     <div class="set7 margin50">
-                        <h1> {!! $package->info_head_5 !!}</h1>
-                        {!! $package->info_detail_5 !!}
+                        <h1> New Line Of Actions</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+
+                        <div class="margin40 row">
+                            <div class="col-md-4">
+                                <h3>Seeking to outperform public markets</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Investing in top-performing buyout manager</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Providing diversified investment focus</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                            </div>
+                        </div>
+
+                        <div class="margin40 row">
+                            <div class="col-md-4">
+                                <h3>Seeking to outperform public markets</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem est nam velit.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Investing in top-performing buyout manager</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis necessitatibus vel ratione dolorum aspernatur officia corrupti, sit qui ad odit iste molestias placeat est aliquid magni vero quis beatae accusantium.</p>
+                            </div>
+
+                            <div class="col-md-4">
+                                <h3>Providing diversified investment focus</h3>
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi magnam non molestias, est sit!</p>
+                            </div>
+                        </div>
+
+                        <p><a href="https://images.unsplash.com/photo-1689895550279-2e64ee1cb432?ixlib=rb-4.0.3&amp;amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;amp;auto=format&amp;amp;fit=crop&amp;amp;w=2940&amp;amp;q=80" target="_blank"><img alt="" src="https://images.unsplash.com/photo-1689895550279-2e64ee1cb432?ixlib=rb-4.0.3&amp;amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;amp;auto=format&amp;amp;fit=crop&amp;amp;w=2940&amp;amp;q=80" style="height:400px; width:100% object-fit:cover" /></a></p>
+
                     </div>
-                
-                    
+
+
                 </div>
             </div>
             <div class="col-md-4">
@@ -74,7 +206,7 @@
                         <div class="set3">
                             <ul>
                                 <li class="up">
-                                    <p ><span class="greendot me-2"></span> IN FINAL CLOSE</p>
+                                    <p><span class="greendot me-2"></span> IN FINAL CLOSE</p>
                                     <p> <span class="blue">INVESTMENT PROCESS</span> </p>
                                 </li>
                                 <li>
@@ -106,21 +238,21 @@
                                     <p>$60,000</p>
                                 </li>
                             </ul>
-                            
+
                             <a href="/user/investment-payment">
                                 <button class="withbackbtn mb-3">
                                     <i class="bi bi-bag text-white"></i>
                                     Purchase Investment
-                                    </button>
+                                </button>
                             </a>
-                             
-                         </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div> 
+</div>
 
 
 
